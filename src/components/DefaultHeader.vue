2ff7b64374961f9c5a3c72e21009f12af22d51e6
hernany <hernanytec777@gmail.com>
2021-06-12 02:51:07 -0300

    construindo base do header

---
+++ src/components/DefaultHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-white flex items-center q-pa-md q-mt-md">
+    <img alt="Rispar logo" src="~assets/logo.png" class="logo" />
+
+    <q-separator vertical inset spaced="24px" size="2px" />
+
+    <span class="text-grey-6">TESTE FRONTEND</span>
+    <span class="text-grey-14 hello-text">
+      Olá, solicite seu crédito Rispar =)
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DefaultHeader"
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  height: auto;
+  max-width: 220px;
+}
+
+.hello-text {
+  font-weight: bold;
+  margin-left: 120px;
+}
+</style>
