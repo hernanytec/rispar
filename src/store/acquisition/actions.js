dfa9ba1a0868100caf91f676755abf232337d995
hernany <hernanytec777@gmail.com>
2021-06-13 03:16:44 -0300

    mock na chamada a api

--- src/store/acquisition/actions.js
+++ src/store/acquisition/actions.js
@@ -7,29 +7,7 @@ export async function simulate({ commit }, form) {
     commit("setLoading", true);
     await delay(500);
     //const { data } = api.post("/acquisition/simulation", form);
-    const data = {
-      id: "8a98cec8-45e5-4b66-9f65-296cf6eaabef",
-      fullname: "hernany",
-      email: "hernanytec777@gmail.com",
-      ltv: 20,
-      contract_value: 10413.872151,
-      net_value: 10000.0,
-      installment_value: 92.628729,
-      last_installment_value: 10506.50088,
-      iof_fee: 205.594708,
-      origination_fee: 208.277443,
-      term: 12,
-      collateral: 27205188,
-      collateral_in_brl: 50000.0,
-      collateral_unit_price: 183788.468898,
-      first_due_date: "2021-07-10",
-      last_due_date: "2022-06-10",
-      interest_rate: 0.89,
-      monthly_rate: 1.238,
-      annual_rate: 15.910479
-    };
-
-    commit("setSimulation", data);
+    // commit("setSimulation", data);
   } catch (err) {
     console.log("aqui", err);
   } finally {
